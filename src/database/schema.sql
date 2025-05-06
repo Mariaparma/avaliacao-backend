9b92a68d0d669727c031eeb69d804f94eef78ef1
maria parma <mariaparma.tds1.senai@gmail.com>
2025-05-06 09:20:52 -0300

    fix: update names in the tables

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -5,28 +5,28 @@ CREATE DATABASE songs;
 
 CREATE TABLE artistas (
     id SERIAL PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
+    name VARCHAR(100) NOT NULL,
     photo VARCHAR(255) NOT NULL,
-    pais VARCHAR(100) NOT NULL
+    country VARCHAR(100) NOT NULL
 );
 
 
 CREATE TABLE albuns (
     id SERIAL PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
+    name VARCHAR(100) NOT NULL,
     photo VARCHAR(255) NOT NULL,
     artista_id INTEGER REFERENCES artistas(id)
 );
 
 
-INSERT INTO artistas (nome, photo, pais) VALUES
+INSERT INTO artistas (name, photo, country) VALUES
 ('Luan Santana', 'https://agenciaamapa.com.br/midias/2024/grandes/up_ag_24060_ea0cfc30-e6e1-aafd-7e3d-402faf8bfc60.jpg', 'Brasil'),
 ('Ana Castela', 'https://www.oktoberfestsantacruz.com.br/wp-content/uploads/2023/10/Ana-Castela_Divulgacao.jpg', 'Brasil'),
 ('OLivia Rodrigo', 'https://f.i.uol.com.br/fotografia/2024/02/24/170882374865da94c473228_1708823748_3x2_md.jpg', 'Estados Unidos'),
 ('Justin Bieber', 'https://upload.wikimedia.org/wikipedia/commons/d/da/Justin_Bieber_in_2015.jpg', 'Canadá');
 
 
-INSERT INTO albuns (nome, photo, artista_id) VALUES
+INSERT INTO albuns (name, photo, artista_id) VALUES
 ('Ao vivo na Lua', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdq79ElhAmbHgk_XzNSRGiV7vArMH7FWyh3g&s', 1),
 ('Boiadeira Internacional', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmod_XBTk48XSBhO0JodijqXE_EqpvDOJDWg&s', 2),
 ('Sour', 'https://upload.wikimedia.org/wikipedia/pt/thumb/7/71/Sour_-_Olivia_Rodrigo.png/250px-Sour_-_Olivia_Rodrigo.png', 3),
